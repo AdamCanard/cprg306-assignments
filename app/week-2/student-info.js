7836091ae850ea88f00e66f5740f3242fd0849e6
AdamCanard <39779534+AdamCanard@users.noreply.github.com>
2024-09-04 13:23:11 -0600

    week 2 commit  with styles

---
+++ app/week-2/student-info.js
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+export function StudentInfo() {
+  return (
+    <div id="border">
+      <h1>ADAM CUNARD</h1>
+      <Link
+        id="border-np"
+        href="https://github.com/AdamCanard/cprg306-assignments"
+      >
+        GitHub!
+      </Link>
+    </div>
+  );
+}
