5bb03a22fdacd4ade6093b9bd194ef54050297c1
AdamCanard <39779534+AdamCanard@users.noreply.github.com>
2024-09-15 19:45:26 -0600

    login and log out

--- app/week-9/page.js
+++ app/week-9/page.js
@@ -1,10 +1,25 @@
 "use client";
 import BasePage from "../basepage";
+import { useState } from "react";
+
 import { useUserAuth } from "./_utils/auth_context";
-import { useEffect } from "react";
+import { ItemList } from "./shopping-list/item-list";
+import MealIdeas from "./shopping-list/meal-ideas";
+import { NewItem } from "./shopping-list/new-item";
+import Items from "./shopping-list/items.json";
 
 export default function Page() {
-  // Use the useUserAuth hook to get the user object and the login and logout functions
+  const [itemList, setItemList] = useState(Items);
+  const [selectedItemName, setSelectedItemName] = useState("");
+
+  function handleItemSelect(e) {
+    setSelectedItemName(emojiRemover(e.name).split(",")[0]);
+  }
+
+  const emojiRemover = (string) => {
+    return string.replace(/[^\p{L}\p{N}\p{P}\p{Z}^$\n]/gu, "");
+  };
+
   const { user, gitHubSignIn, firebaseSignOut } = useUserAuth();
 
   const SignIn = async () => {
@@ -15,12 +30,30 @@ export default function Page() {
     await firebaseSignOut();
   };
 
+  function Login({ children, user }) {
+    return (
+      <>
+        {user ? (
+          <>{children}</>
+        ) : (
+          <>
+            <div onClick={SignIn}> Login</div>
+          </>
+        )}
+      </>
+    );
+  }
+
   return (
     <BasePage>
-      <div className="flex gap-2">
-        <div onClick={SignIn}> Login</div>
+      <Login user={user}>
+        <div className="flex flex-row w-[90%] items-center justify-around">
+          <ItemList items={itemList} onItemSelect={handleItemSelect} />
+          <MealIdeas ingredient={selectedItemName} />
+          <NewItem getter={itemList} setter={setItemList} />
+        </div>
         <div onClick={SignOut}> Signout</div>
-      </div>
+      </Login>
     </BasePage>
   );
 }
